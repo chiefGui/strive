d86e44251ff2e978959b868b87830ecc5b1f22e7
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2018-02-28 02:18:07 -0300

    More updates

--- StartServer.bat
+++ StartServer.bat
@@ -1,2 +1,2 @@
-"C:\UE4\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\Mantiquen.uproject" PLANEGAME -server -log -nosteam
+"C:\UE4\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\Mantiquen.uproject" Plane -server -log -nosteam
 exit /B
