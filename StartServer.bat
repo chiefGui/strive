8158da64dfe2150fb8b7ef3503ce6c2d886b3c6d
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2018-03-01 00:08:50 -0300

    Change server's load map

--- StartServer.bat
+++ StartServer.bat
@@ -1,2 +1,2 @@
-"C:\UE4\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\Mantiquen.uproject" Plane -server -log -nosteam
+"C:\UE4\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\Mantiquen.uproject" Planet -server -log -nosteam
 exit /B
