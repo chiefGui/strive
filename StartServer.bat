7868d03082fb1732940eee8a39dd68fa05f16ab7
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2018-03-06 02:06:41 -0300

    Fix a few issues on packaging

--- StartServer.bat
+++ StartServer.bat
@@ -1,1 +1,1 @@
-"C:\UE4\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\Mantiquen.uproject" Planet -server -log
+"C:\UE4\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\Mantiquen.uproject" Planet -server -log -nosteam
