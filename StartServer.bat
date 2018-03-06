9717095c046195eb9960f1df1756cf24cbbed1f2
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2018-03-06 00:29:27 -0300

    Add Lonsdaleite Shield and Physical Defense status

--- StartServer.bat
+++ StartServer.bat
@@ -1,2 +1,1 @@
-"C:\UE4\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\Mantiquen.uproject" Planet -server -log -nosteam
-exit /B
+"C:\UE4\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\Mantiquen.uproject" Planet -server -log
