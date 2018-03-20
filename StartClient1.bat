46c2317183a348db1dc4887b59b57f4324089b82
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2018-03-20 16:43:42 -0300

    Sanitize bat files

--- StartClient1.bat
+++
@@ -1,2 +0,0 @@
-TIMEOUT /T 6
-"C:\UE4\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\Mantiquen.uproject" StartMenu -game -ResX=800 -ResY=900 -WinX=0 -WinY=20 -log
--- StartClient2.bat
+++
@@ -1,2 +0,0 @@
-TIMEOUT /T 6
-"C:\UE4\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\Mantiquen.uproject" 192.168.0.27 -game -ResX=800 -ResY=900 -WinX=0 -WinY=20 -log -nosteam
