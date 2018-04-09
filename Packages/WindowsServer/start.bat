b21c0e6aa16d810244ea0a543d5d2dfed790193e
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2018-04-09 13:56:15 -0300

    Fix players visibility when two or more are within a bush

--- Packages/WindowsServer/start.bat
+++ Packages/WindowsServer/start.bat
@@ -1,4 +1,4 @@
 SET serverLocation=%~dp0WindowsServer
 SET fileName=StriveServer.exe
 
-"%serverLocation%\%filename%" /Game/Levels/Plane/Planet -log
+"%serverLocation%\%filename%" /Game/Levels/Westgrim/Landscape/Westgrim -log
--- StartShippingRemoteServer.bat
+++ StartShippingRemoteServer.bat
@@ -1,4 +1,4 @@
-SET location=%~dp0Packages\WindowsServer\
-SET fileName=StriveServer.exe
+SET location=%~dp0Binaries\Win64
+SET fileName=StriveServer-Win64-Shipping.exe
 
-"%location%\%filename%" /Game/Levels/Plane/Planet -log
+"%location%\%filename%" Westgrim -log
