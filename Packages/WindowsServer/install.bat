fb1d4c77b861832b43ba1f8a186a125836a4911e
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2018-04-25 13:05:22 -0300

    Improve GameLift's install script

--- Packages/WindowsServer/install.bat
+++ Packages/WindowsServer/install.bat
@@ -3,7 +3,7 @@ SET dependenciesLocation=%currentLocation%Dependencies
 SET logsLocation=%currentLocation%Logs
 
 SET vcredistFileName=vcredist_x64.exe
-SET directxFileName=dxsetup.exe
+SET directxFileName=dxwebsetup.exe
 
-"%dependenciesLocation%\%vcredistFileName%" /install /passive /norestart /quiet
-& "%dependenciesLocation%\%directxFileName%" /install /silent
+"%dependenciesLocation%\%vcredistFileName%" /q
+"%dependenciesLocation%\%directxFileName%" /q
