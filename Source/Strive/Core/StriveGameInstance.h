d5527298c4c5ce8178d34f52e97188708d62b877
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2018-04-28 00:46:37 -0300

    Update few things

---
+++ Source/Strive/Core/StriveGameInstance.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "StriveGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STRIVE_API UStriveGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
